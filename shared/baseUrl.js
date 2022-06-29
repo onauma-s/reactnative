55844f7184d91aab1964b636b55a74533a9bf6f0
Onauma Sukkarat <o5na.puchii@gmail.com>
2022-06-28 21:15:29 -0700

    Assignment week 2

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,4 @@
-//export const baseUrl = 'http://192.168.1.10:3001/';
+export const baseUrl = 'http://192.168.1.10:3001/';
 
-export const baseUrl = 'http://172.17.77.77:3001/';
+// 7sea wifi
+//export const baseUrl = 'http://172.17.77.77:3001/';
